fb67e017dfb7b74e11ce5a1860aaedcc35a5ac2e
ericreilly-ai <mlgreillyxbl@gmail.com>
2025-10-31 20:33:01 -0700

    Create outputs.tf

---
+++ infra/outputs.tf
@@ -0,0 +1,14 @@
+output "cluster_name" {
+  description = "Name of the EKS cluster"
+  value       = module.eks.cluster_id
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint of the EKS cluster"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data for the cluster"
+  value       = module.eks.cluster_certificate_authority_data
+}
